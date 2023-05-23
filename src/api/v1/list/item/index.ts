385d1dcfb4878bce3d6e0e3cc3c1d404bf111db6
Daniel Stehura <stehura.daniel@gmail.com>
2023-05-23 19:22:58 +0200

    Items router

---
+++ src/api/v1/list/item/index.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express'
+
+import * as postItem from './post.item'
+import * as deleteItem from './delete.item'
+
+// middlewares
+import validationMiddleware from '../../../../middlewares/validationMiddleware'
+
+const router = Router()
+
+export default () => {
+	// Create a new item
+	router.post(
+		'/',
+		validationMiddleware(postItem.schema),
+		postItem.workflow
+	)
+	// Delete a item
+	router.delete(
+		'/:itemID',
+		validationMiddleware(deleteItem.schema),
+		deleteItem.workflow
+	)
+
+	return router
+}
